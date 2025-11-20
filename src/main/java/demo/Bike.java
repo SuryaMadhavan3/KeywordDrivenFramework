89286bb0126056bffe8d14d071dfc0206692339c
SuryaM <sivasuryanarayananm@ugf360.com>
2025-11-20 15:35:19 +0530

    20/11

--- src/main/java/demo/Bike.java
+++ src/main/java/demo/Bike.java
@@ -5,18 +5,16 @@ public class Bike implements TrafficRules{
 	@Override
 	public void Green() {
 		System.out.println("Bike - Start");
-		
 	}
 	
 	@Override
 	public void Red() {
 		System.out.println("Bike - Stop");
-		
 	}
 	
 	@Override
 	public void Yellow() {
 		System.out.println("Bike - Ready");
-		
 	}
+	
 }
